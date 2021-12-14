031cc48ae40ff1dfec106df5f42f359060c31fc8
unknown <ayoub6nafil10@gmail.com>
2021-12-14 21:14:34 +0100

    fff

--- app.js
+++ app.js
@@ -22,6 +22,6 @@ app.use('/salles',salleRouter)
 
 
 
-app.listen(9000, () => {
-    console.log('Server started')
-})
+app.listen(process.env.PORT || 3000, function(){
+    console.log("Express server listening on port %d in %s mode", this.address().port, app.settings.env);
+  });
